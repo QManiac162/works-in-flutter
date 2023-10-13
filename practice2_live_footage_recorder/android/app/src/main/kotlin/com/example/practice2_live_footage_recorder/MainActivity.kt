f0255e4c9d3a3f95a0a9241e1037c426ec7e7c8e
QManiac162 <quantummaniac609@gmail.com>
2023-10-13 11:40:25 +0530

    completed practice-2!

---
+++ practice2_live_footage_recorder/android/app/src/main/kotlin/com/example/practice2_live_footage_recorder/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.practice2_live_footage_recorder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ practice2_live_footage_recorder/lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+late List<CameraDescription> _cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  _cameras = await availableCameras();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  late CameraController cameraController;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    cameraController = CameraController(_cameras[0], ResolutionPreset.high);
+    cameraController.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+          // Handle access errors here.
+            print('user denied camera access');
+            break;
+          default:
+          // Handle other errors here.
+            print('handle other errors');
+            break;
+        }
+      }
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      body: CameraPreview(cameraController),
+    );
+  }
+}
